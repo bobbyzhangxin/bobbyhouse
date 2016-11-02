11ef4589d1e387592d4507ec0aab29b8d2a47581
bobbyzhangxin <1181867033@qq.com>
2016-11-02 23:34:11 +0800

    123

---
+++ 课件/课件/index.js
@@ -0,0 +1,46 @@
+ var title_nav = document.querySelector("#logo");
+
+
+var aaa = "hahaha"
+
+
+
+
+function changeFont () {
+	var asd = "123";
+
+	console.log(title_nav.innerHTML)
+	var content = title_nav.innerHTML;
+	title_nav.innerHTML = "chrome"
+	var content = tltle_nav.innerHTML;
+};
+
+
+
+title_nav.onclick = function() {
+	console.log(asd);
+};
+
+
+
+function change () {
+
+	var a = "aaaaaaaaa"
+
+	function add () {
+
+		var q = "qqqqqqq";
+
+		alert(asd);
+
+	};
+
+
+	add ();
+
+
+};
+
+
+
+change ();
